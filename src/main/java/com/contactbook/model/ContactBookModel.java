769f3a368039459d337559d34d9f9ed10b6f6162
Mahmoud Ahmed <ma7moudbahnasawy@outlook.com>
2024-03-20 14:21:56 +0200

    feat: Implement ContactBookModel class in model package.

--- src/main/java/com/contactbook/model/ContactBookModel.java
+++ src/main/java/com/contactbook/model/ContactBookModel.java
@@ -1,5 +1,26 @@
 package com.contactbook.model;
 
+import java.util.ArrayList;
+
 public class ContactBookModel {
-    
+    private final String[] headers = { "Name", "Email", "Phone Number" };
+    private final ArrayList<Contact> contacts = new ArrayList<>();
+
+    public String[][] getContacts() {
+        return contacts.stream()
+                .map(Contact::toArray)
+                .toArray(String[][]::new);
+    }
+
+    public String[] getHeaders() {
+        return this.headers;
+    }
+
+    public void addContact(String name, String email, String phoneNumber) {
+        contacts.add(new Contact(name, email, phoneNumber));
+    }
+
+    public void removeContact(int index) {
+        contacts.remove(index);
+    }
 }
