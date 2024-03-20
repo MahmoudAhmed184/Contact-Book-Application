1af949dd16c20f01e4c983b48e9bdf548c61fdde
Mahmoud Ahmed <ma7moudbahnasawy@outlook.com>
2024-03-20 13:51:43 +0200

    feat: Implement Contact class in model package.

--- src/main/java/com/contactbook/model/Contact.java
+++ src/main/java/com/contactbook/model/Contact.java
@@ -1,5 +1,41 @@
 package com.contactbook.model;
 
 public class Contact {
-    
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phone) {
+        this.phoneNumber = phone;
+    }
+
+    public String[] toArray() {
+        return new String[] { name, email, phoneNumber };
+    }
+
+    public Contact(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phone;
+    }
 }
