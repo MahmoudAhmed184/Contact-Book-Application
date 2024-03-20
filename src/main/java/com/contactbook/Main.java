cd71067d114aa4c1fa25b7b6bc610327183270d3
Mahmoud Ahmed <ma7moudbahnasawy@outlook.com>
2024-03-20 14:45:38 +0200

    test: Verify the functionality of the Contact Book Application in Main class.

--- src/main/java/com/contactbook/Main.java
+++ src/main/java/com/contactbook/Main.java
@@ -1,5 +1,10 @@
 package com.contactbook;
 
+import com.contactbook.controller.ContactBookController;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        ContactBookController contactBookController = new ContactBookController();
+        contactBookController.initialize();
+    }
 }
